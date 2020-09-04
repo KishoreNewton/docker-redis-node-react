3a78ed0f83e119635db8aa85639c346c302bd263
Kishore Newton <kishorenke@gmail.com>
2020-09-05 01:48:26 +0530

    fixed test

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import OtherPage from './otherPage'
+import Fib from './Fib'
 
 class App extends Component {
   render() {
-    return (
+    return(
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-
-            <h1>KUBERNETES!</h1>
-            <h1 className="App-title">Fib Calculator</h1>
+          <h1>KUBERNETES MATE</h1>
+          <header>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Route path="/otherPage" component={OtherPage} />
           </div>
         </div>
       </Router>
-    );
+    )
   }
 }
 
